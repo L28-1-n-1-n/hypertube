aef3f775c77c929f6eb8f46dd61886755c0b0888
L28-1-n-1-n <zto2.71828@gmail.com>
2020-07-07 19:19:16 +0100

    changing keys

--- config/keys.js
+++ config/keys.js
@@ -1,14 +1,17 @@
 const FORTYTWO = {
-    clientID: '34163cd6c87885d9996267d8e9777e2ae0b8eb83e0bcdc89b0fc3976169fb918',
-    clientSecret: '83d56a400a0d29c394d12c53334238b5505742f7cfe23606d7fb868272c2416f'
-}
+  fortyTwoClientID:
+    '8d8755781b947dc57742bb1a4683f46c50ecfb8f0153950e16c92b1d12320a9c',
+  fortyTwoClientSecret:
+    '15f27d35517d81c1f225508a2480056261457bec17a9e695402dd8aeb668a8b6',
+};
 
 const GITHUB = {
-    clientID: '9c4ca4e91c36b7692f63',
-    clientSecret: '5ba9d331928e964920bbd8dd754f0382d23b0b38'
-}
-
-module.exports = {
-    FORTYTWO,
-    GITHUB
+  githubClientID: 'Iv1.b0d21b8102c287bd',
+  githubClientSecret: '1105833ef4d6825c8628b1f12689aa7458a25195',
 };
+
+const KEYS = {
+  ...FORTYTWO,
+  ...GITHUB,
+};
+module.exports = KEYS;
